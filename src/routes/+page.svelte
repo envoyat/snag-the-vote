90a6a52c25f668d9e5a4307cc287c802eb55b0de
SeanEnv <sean.clark@envoyat.com>
2024-09-07 01:04:52 +1000

    Add initial basic map view

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,13 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+
+</script>
+
+<main data-sveltekit-preload-data="false">
+  <h1>Snag a vote!</h1>
+
+  <p>Open the <a href="/map">Snag a vote</a> map!</p>
+</main>
+
+<style>
+
+</style>
---
+++ src/routes/map/+page.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+  import '@arcgis/core/assets/esri/themes/light/main.css';
+  import MapView from '@arcgis/core/views/MapView';
+  import WebMap from '@arcgis/core/WebMap';
+
+  const WEBMAP_ID = '88d2b75f8cd24ec0bbfc0d75c906e83b';
+
+  function createMap(node: HTMLDivElement) {
+    const webMap = new WebMap({
+      portalItem: {
+        id: WEBMAP_ID,
+      }
+    });
+
+    const view = new MapView({
+      container: node,
+      map: webMap,
+    });
+
+    return {
+      destroy() {
+        view.destroy();
+      }
+    };
+  }
+</script>
+
+<main class="h-screen">
+  <h1>Snag a vote!</h1>
+  <div id="viewDiv" use:createMap></div>
+</main>
+
+<style>
+  #viewDiv {
+    width: 100%;
+    height: 80%;
+  }
+</style>
